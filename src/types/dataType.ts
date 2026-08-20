0da85ef25ea768a12d1590ee3d5faf0e6d96e593
sandro-dev26 <sadrokakhidze@gmail.com>
2026-08-21 02:09:33 +0400

    Add data.json examples and types

---
+++ src/types/dataType.ts
@@ -0,0 +1,11 @@
+export interface ScheduleType {
+  id: string; // id should be all lowercase, eg: morningcoding
+  title: string; // title can be anything you want, eg: "Morning Coding"
+  time: {
+    start: string;
+    end: string;
+  }; // write strings like this: "09:30", "13:00" or not, but don't blame me afterwards
+  weekdayAll: boolean; // is it all week or no
+  weekdays: string[]; // week days (capitalized) or ["All"] if all, eg: ["Friday", "Saturday", "Sunday"] IT'S FRIDAY AGAIN! SUNDAY, SATURDAY, WHAT?! IT'S FRIDAY AGAIN... ZANDVOORT IS HEREEE!!!
+  isImportant: boolean; // is it important or no for you
+}
